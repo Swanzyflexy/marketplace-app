95747a4176f3eeccc5826d9278568e47fb91e0bb
Swanzyflexy <swanzyflexy@gmail.com>
2024-01-05 20:42:27 +0530

    Major and FInal Update, All Done

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -25,11 +25,135 @@ class FrontController extends AbstractController
 
         $categories = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
 
-        // dd($ads, $categories);
-
         return $this->render('pages/index.html.twig', [
           'ads' => $ads,
           'categories' => $categories,
         ]);
+    }
+
+    #[Route('/category/{id}', name: 'app_category_show', methods: ['GET'])]
+    public function ShowCategories(AdCategory $category)
+    {
+        // $category = $this->em->getRepository(AdCategory::class)->find($id);
+        $subCategories = $category->getSubCategories();
+        $ads = [];
+        foreach ($subCategories as $subCategory) {
+            $subCategoryAds = $this->em->getRepository(Ad::class)->findBy(['category' => $subCategory]);
+            $ads = array_merge($ads, $subCategoryAds);
+        }
+
+        // Get all parent categories
+        $parentCategories = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
+
+        return $this->render('pages/display-Categories.html.twig', [
+          'ads' => $ads,
+          'subCategories' => $subCategories,
+          'category' => $category,
+          'parentCategories' => $parentCategories,
+          ]);
+    }
+
+    #[Route('/category/autos', name: 'app_electronics', methods: ['GET'])]
+    public function ShowElectronics()
+    {
+        $category = $this->em->getRepository(AdCategory::class)->findOneBy(['name' => 'Electronics']);
+        if ($category->getParentCategory() === null) {
+            // $category is a parent category
+            $subCategories = $category->getSubCategories();
+            $ads = [];
+            foreach ($subCategories as $subCategory) {
+                $subCategoryAds = $this->em->getRepository(Ad::class)->findBy(['category' => $subCategory]);
+                $ads = array_merge($ads, $subCategoryAds);
+            }
+        } else {
+            // $category is a sub category
+            $parentCategory = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
+            $ads = $this->em->getRepository(Ad::class)->findBy(['category' => $category]);
+        }
+
+        return $this->render('pages/display-Categories.html.twig', [
+          'ads' => $ads,
+          'subCategories' => $subCategories,
+          'category' => $category,
+          'parentCategory' => $parentCategory,
+          ]);
+    }
+
+    #[Route('/category/autos', name: 'app_estates', methods: ['GET'])]
+    public function ShowRealEstates()
+    {
+        $category = $this->em->getRepository(AdCategory::class)->findOneBy(['name' => 'Real Estate']);
+        if ($category->getParentCategory() === null) {
+            // $category is a parent category
+            $subCategories = $category->getSubCategories();
+            $ads = [];
+            foreach ($subCategories as $subCategory) {
+                $subCategoryAds = $this->em->getRepository(Ad::class)->findBy(['category' => $subCategory]);
+                $ads = array_merge($ads, $subCategoryAds);
+            }
+        } else {
+            // $category is a sub category
+            $parentCategory = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
+            $ads = $this->em->getRepository(Ad::class)->findBy(['category' => $category]);
+        }
+
+        return $this->render('pages/display-Categories.html.twig', [
+          'ads' => $ads,
+          'subCategories' => $subCategories,
+          'category' => $category,
+          'parentCategory' => $parentCategory,
+          ]);
+    }
+
+    #[Route('/category/autos', name: 'app_autos', methods: ['GET'])]
+    public function ShowAutos()
+    {
+        $category = $this->em->getRepository(AdCategory::class)->findOneBy(['name' => 'Vehicles']);
+        if ($category->getParentCategory() === null) {
+            // $category is a parent category
+            $subCategories = $category->getSubCategories();
+            $ads = [];
+            foreach ($subCategories as $subCategory) {
+                $subCategoryAds = $this->em->getRepository(Ad::class)->findBy(['category' => $subCategory]);
+                $ads = array_merge($ads, $subCategoryAds);
+            }
+        } else {
+            // $category is a sub category
+            $parentCategory = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
+            $ads = $this->em->getRepository(Ad::class)->findBy(['category' => $category]);
+        }
+
+        return $this->render('pages/display-Categories.html.twig', [
+          'ads' => $ads,
+          'subCategories' => $subCategories,
+          'category' => $category,
+          'parentCategory' => $parentCategory,
+          ]);
+    }
+
+    #[Route('/category/autos', name: 'app_homes', methods: ['GET'])]
+    public function ShowHomes()
+    {
+        $category = $this->em->getRepository(AdCategory::class)->findOneBy(['name' => 'Homes']);
+        if ($category->getParentCategory() === null) {
+            // $category is a parent category
+            $subCategories = $category->getSubCategories();
+            $ads = [];
+            foreach ($subCategories as $subCategory) {
+                $subCategoryAds = $this->em->getRepository(Ad::class)->findBy(['category' => $subCategory]);
+                $ads = array_merge($ads, $subCategoryAds);
+            }
+        } else {
+            // $category is a sub category
+            $parentCategory = $this->em->getRepository(AdCategory::class)->findBy(['parentCategory' => null]);
+            $ads = $this->em->getRepository(Ad::class)->findBy(['category' => $category]);
+        }
+
+        return $this->render('pages/display-Categories.html.twig', [
+          'ads' => $ads,
+          'subCategories' => $subCategories,
+          'category' => $category,
+          'parentCategory' => $parentCategory,
+          ]);
     }
 }
